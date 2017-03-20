8762739d14cc2b4aa92cabca65bd1ab4e037a1b9
aripddev <dev@aripd.com>
2017-03-20 14:35:32 +0300

    update

--- src/main/java/com/aripd/ecommerce/entity/ProductEntity.java
+++ src/main/java/com/aripd/ecommerce/entity/ProductEntity.java
@@ -153,7 +153,7 @@ public class ProductEntity extends AbstractEntity {
     public List<ImageEntity> getImages() {
         return images
                 .stream()
-                .filter(i -> i.getImageType().equals(ImageType.SHOWCASE))
+//                .filter(i -> i.getImageType().equals(ImageType.SHOWCASE))
                 .sorted((p1, p2) -> Integer.compare(p1.getSortOrder(), p2.getSortOrder()))
                 .collect(Collectors.toList());
     }
