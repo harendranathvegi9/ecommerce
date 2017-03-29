9ab1a506d28d6e47c7c190699e43a676f0fca2e1
aripddev <dev@aripd.com>
2017-03-29 23:38:33 +0300

    update

--- src/main/java/com/aripd/ecommerce/view/administrator/ReportController.java
+++ src/main/java/com/aripd/ecommerce/view/administrator/ReportController.java
@@ -2,10 +2,9 @@ package com.aripd.ecommerce.view.administrator;
 
 import com.aripd.ecommerce.model.ReportFormModel;
 import com.aripd.ecommerce.service.SalelineService;
+import com.aripd.util.MessageUtil;
 import java.io.Serializable;
 import java.util.Calendar;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.event.ActionEvent;
 import javax.faces.view.ViewScoped;
@@ -16,17 +15,18 @@ import org.primefaces.model.chart.PieChartModel;
 @Named
 @ViewScoped
 public class ReportController implements Serializable {
-
-    private static final Logger LOG = Logger.getLogger(ReportController.class.getName());
-
+    
     private PieChartModel pieModel1;
     private PieChartModel pieModel2;
-
+    
     private ReportFormModel reportFormModel;
-
+    
     @Inject
     private SalelineService salelineService;
-
+    
+    @Inject
+    MessageUtil messageUtil;
+    
     public ReportController() {
         reportFormModel = new ReportFormModel();
         Calendar cal = Calendar.getInstance();
@@ -34,66 +34,67 @@ public class ReportController implements Serializable {
         cal.add(Calendar.DAY_OF_MONTH, -1);
         reportFormModel.setStart(cal.getTime());
     }
-
+    
     @PostConstruct
     public void init() {
         createPieModels();
     }
-
+    
     public void doDrawChart(ActionEvent actionEvent) {
-        LOG.log(Level.INFO, "start: {0}", reportFormModel.getStart());
-        LOG.log(Level.INFO, "end: {0}", reportFormModel.getEnd());
         createPieModels();
     }
-
+    
     private void createPieModels() {
         createPieModel1();
         createPieModel2();
     }
-
+    
     private void createPieModel1() {
         pieModel1 = new PieChartModel();
-
+        
         pieModel1.setData(salelineService.getSalesRevenueData(reportFormModel));
-
-        pieModel1.setTitle("Sales Revenue");
-        pieModel1.setLegendPosition("w");
+        
+        pieModel1.setTitle(messageUtil.getI18nResource("Sales Revenue"));
+        pieModel1.setLegendPosition("e");
+        pieModel1.setFill(true);
+        pieModel1.setShowDataLabels(true);
+        pieModel1.setDiameter(150);
     }
-
+    
     private void createPieModel2() {
         pieModel2 = new PieChartModel();
-
+        
         pieModel2.setData(salelineService.getSalesVolumeData(reportFormModel));
-
-        pieModel2.setTitle("Sales Volume");
+        
+        pieModel2.setTitle(messageUtil.getI18nResource("Sales Volume"));
         pieModel2.setLegendPosition("e");
         pieModel2.setFill(false);
         pieModel2.setShowDataLabels(true);
         pieModel2.setDiameter(150);
     }
-
+    
     public ReportFormModel getReportFormModel() {
         return reportFormModel;
     }
-
+    
     public void setReportFormModel(ReportFormModel reportFormModel) {
         this.reportFormModel = reportFormModel;
     }
-
+    
     public PieChartModel getPieModel1() {
         return pieModel1;
     }
-
+    
     public void setPieModel1(PieChartModel pieModel1) {
         this.pieModel1 = pieModel1;
     }
-
+    
     public PieChartModel getPieModel2() {
         return pieModel2;
     }
-
+    
     public void setPieModel2(PieChartModel pieModel2) {
         this.pieModel2 = pieModel2;
     }
-
+    
 }
